371ae798db7deecee8b1da24fb1716e4a06daccc
Peach <scdzwyxst@gmail.com>
2016-01-07 10:56:06 +0800

    Add in-making page and 404 page

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.status(404).render('404.jade');
 });
 
 // error handlers
